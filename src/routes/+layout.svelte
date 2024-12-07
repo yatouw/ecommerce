a9500957e08c33cefb13cab2ba5565424682d160
Yatouwu <154877343+Yatouwu@users.noreply.github.com>
2024-12-08 04:53:39 +0800

    basic login and register implementation

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,16 +4,12 @@
 	import AuthNavbar from '$lib/components/AuthNavbar.svelte';
 	import { page } from '$app/stores';
 
-	// Reactive tracking of the current route
 	$: currentPage = $page.url.pathname;
 </script>
 
 {#if currentPage.startsWith('/userhome')}
-	<!-- Show AuthNavbar for authenticated pages -->
 	<AuthNavbar />
 {:else}
-	<!-- Show Navbar for public pages -->
 	<Navbar />
 {/if}
-
 <slot />
